3d207bf67cbec1074a6855575b537071b1baacad
Louay Benali <louaybenali133@gmail.com>
2025-03-19 22:54:37 +0100

    Generer un JWT

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import { register, handleLogin } from '../controllers/authController.js';
 import { refreshTokenController } from '../controllers/refreshController.js'; 
-import { authorize } from '../middleware/auth.js';
 
 
 const router = express.Router();
